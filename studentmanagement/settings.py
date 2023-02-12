4944712b3fde1f42f15073a35783ed508cc6ff69
Joe Muraguri <joe.crrm@gmail.com>
2023-02-12 15:21:22 +0300

    added permissions

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-86%&k)e6xcj58=cas0i9(qfa^_!nop8wpuv7-y1!hppo^@f^c(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
