c96795f2fd49112c8b9f9320b9bfcfc8fe5651c7
Joe Muraguri <joe.crrm@gmail.com>
2023-02-12 16:02:50 +0300

    added new environment

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -20,8 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-86%&k)e6xcj58=cas0i9(qfa^_!nop8wpuv7-y1!hppo^@f^c('
+# SECRET_KEY = 'django-insecure-86%&k)e6xcj58=cas0i9(qfa^_!nop8wpuv7-y1!hppo^@f^c('
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -79,10 +85,29 @@ WSGI_APPLICATION = 'studentmanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DB_NAME=students
+# DB_USER=postgres
+# DB_PASSWORD=postgres
+# DB_HOST=localhost
+# DB_PORT=5432
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"), 
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"), 
+        'PORT': env("DB_PORT"),
     }
 }
 
